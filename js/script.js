936f555ca2a6b38b7596f35851ca4bed0e987386
NovikoffOleh <novikoff10000@gmail.com>
2022-12-13 20:27:28 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 const weatherBlok = document.querySelector('#weather');
+
 async function loadWeather (e){
     weatherBlok.innerHTML =`
     <div class="weather__loading">
     <img src="img/ZWdx.gif" alt="Завантаження..">
     </div>`;
+
+const server = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Kyiv&appid=e5e67bd5c36ddb64a388f957cf7d7af6'
+const response =await fetch (server, {
+    method: 'GET',
+});
+
+const responseResult = await.response.json();
+if (response.ok) {
+   getWeather(responseResult);
+} else {
+    weatherBlok.innerHTML = responseResult.message;
 }
+}
+
+
+
 if (weatherBlok) {
     loadWeather ();
 }
