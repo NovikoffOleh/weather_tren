69e82a19c22b95d057b639e11c6cc7213256cd84
NovikoffOleh <novikoff10000@gmail.com>
2022-12-13 20:38:20 +0200

    add

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const weatherBlok = document.querySelector('#weather');
 async function loadWeather (e){
     weatherBlok.innerHTML =`
     <div class="weather__loading">
-    <img src="img/ZWdx.gif" alt="Завантаження..">
+    <img src="img/ZWdx.gif" width="150px" height="100px" alt="Завантаження..">
     </div>`;
 
 const server = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Kyiv&appid=e5e67bd5c36ddb64a388f957cf7d7af6'
@@ -11,7 +11,7 @@ const response =await fetch (server, {
     method: 'GET',
 });
 
-const responseResult = await.response.json();
+const responseResult = await response.json();
 if (response.ok) {
    getWeather(responseResult);
 } else {
@@ -19,6 +19,14 @@ if (response.ok) {
 }
 }
 
+function getWeather(data){
+    console.log(data)
+    const location = data.name;
+    const temp = Math.round(data.main.temp);
+    const feelslike = Math.round(data.main.feels_like);
+    const weatherStatus = data.weather[0].main;
+    const weatherIcon = data.weather[0].icon;
+}
 
 
 if (weatherBlok) {
